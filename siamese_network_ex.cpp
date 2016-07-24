5c11e02a8d7c922f76d433512d23c19bcb113c28
Philip Tran <ptran516@gmail.com>
2016-07-24 11:39:29 -0400

    Add additional documentation and small fixes.

--- siamese_network_ex.cpp
+++ siamese_network_ex.cpp
@@ -6,47 +6,83 @@
 
 // ---------------------------------------------------------------------------
 
-typedef std::pair<dlib::matrix<unsigned char>,dlib::matrix<unsigned char> > image_pair;
+// Here, let's typedef a sample type for the siamese network. A sample is simply
+// a pair of byte images.
+typedef std::pair<dlib::matrix<unsigned char>,dlib::matrix<unsigned char>> image_pair;
 
+// The following demonstrates how a user can define their own custom input layer
+// to accommodate whatever data they expect. This layer's main task is to
+// translate your iterable data samples into a [dlib::resizeable_]tensor. Below,
+// the <interface> tag will denote things that are required to be defined for
+// the dlib input layer interface. This interface is more explicitly shown in
+// dlib's EXAMPLE_INPUT_LAYER code.
 class input_image_pair {
 public:
-    const static unsigned int sample_expansion_factor = 2;
-    // We define the input type as a pointer to a matrix. This allows
-    // construction of pairwise examples without copying input data.
-    typedef image_pair input_type;
+    // The sample expansion factor is the ratio between each sample and the
+    // number of samples that will appear in the output data tensor. Since each
+    // sample is a pair of images, let us define the sample expansion factor to
+    // be 2.
+    const static unsigned int sample_expansion_factor = 2; // <interface>
+    typedef image_pair input_type;                         // <interface>
 
-    template <typename input_iterator>
+    // This function defines how we take an iterable set of inputs and convert
+    // it to a data tensor.
+    template <typename input_iterator>                     // <interface>
     void to_tensor(
         input_iterator ibegin,
         input_iterator iend,
         dlib::resizable_tensor& data
     ) const
     {
+        // These asserts enforce different things we expect from our data.
+        // While not required, this is good practice for catching silly
+        // mistakes.
+        DLIB_CASSERT(std::distance(ibegin, iend) > 0,"");
+
+        // First, we extract the shape each image, and use it to define the size
+        // of the data tensor.
         const long nr = ibegin->first.nr();
         const long nc = ibegin->first.nc();
         data.set_size(std::distance(ibegin, iend)*2, 1, nr, nc);
 
+        for (auto i = ibegin; i != iend; ++i) {
+            DLIB_CASSERT(i->first.nc() == nc && i->second.nc() == nc &&
+                         i->first.nr() == nr && i->second.nr() == nr,"");
+        }
+
+
+        // To get the actual data elements of the tensor, we can call the host()
+        // function. This returns a pointer to the first float element. The
+        // tensor is structured in the following order: columns, rows, columns,
+        // images. Therefore, the offset from image to image is
+        // columns*rows*channels, channel to channel is columns*rows, etc...
         long offset = nr*nc;
         float* data_ptr = data.host();
         for (auto i = ibegin; i != iend; ++i) {
             for (long r = 0; r < nr; ++r) {
                 for (long c = 0; c < nc; ++c) {
+                    // Copy the data pointer while also iterating to the next
+                    // element.
                     float* p = data_ptr++;
                     *p = static_cast<float>(i->first(r,c))/256.0;
                     *(p+offset) = static_cast<float>(i->second(r,c))/256.0;
                 }
             }
+            // In the loop above, we've already populated each image pair, so
+            // here we jump to the next image pair.
             data_ptr += offset;
         }
     }
 };
 
-void serialize(const input_image_pair& item, std::ostream& out)
+// Here, we provide functions for saving, loading, and being verbose about our
+// custom input layer.
+void serialize(const input_image_pair& item, std::ostream& out) // <interface>
 {
     dlib::serialize("input_image_pair", out);
 }
 
-void deserialize(input_image_pair& item, std::istream& in)
+void deserialize(input_image_pair& item, std::istream& in)      // <interface>
 {
     std::string version;
     dlib::deserialize(version, in);
@@ -55,48 +91,66 @@ void deserialize(input_image_pair& item, std::istream& in)
     }
 }
 
-std::ostream& operator<<(std::ostream& out, const input_image_pair& item)
+std::ostream& operator<<(std::ostream& out, const input_image_pair& item) // <interface>
 {
     out << "input_image_pair";
     return out;
 }
 
-void to_xml(const input_image_pair& item, std::ostream& out)
+void to_xml(const input_image_pair& item, std::ostream& out)   // <interface>
 {
     out << "<input_image_pair/>";
 }
 
 // ---------------------------------------------------------------------------
 
+// The following demonstrates how a user can define their own custom loss
+// layers. This layer will be responsible for taking the network output and
+// computing a loss and gradient for element in the output. Below, the
+// <interface> tag will denote things that are required to be defined for the
+// dlib loss layer interface. This interface is more explicitly shown in dlib's
+// EXAMPLE_LOSS_LAYER_ code.
 class loss_contrastive_ {
 public:
-    // Here, defining the sample_expansion_factor as 2 makes any network
-    // utilizing this loss layer to consider pairs of inputs per label.
+    // The sample expansion factor here defines the ratio of samples to
+    // labels. Since each sample pair is associated with a label, the factor is
+    // set to 2.
     const static unsigned int sample_expansion_factor = 2;
     typedef unsigned char label_type;
 
+    // Contrastive loss is defined as
+    //    loss = 0.5/num_samples * sum(y*d*d + (1-y)*pow(max(margin-d,0),2))
+    // where d is the euclidean distance between two samples.
     loss_contrastive_(double margin_=1.0, double thresh_=1.0)
         : margin(margin_), thresh(thresh_)
     { }
 
+    // The label threshold here just defines a distance with which we say image
+    // pairs are the same or not.
     double get_label_threshold() const;
     void set_label_threshold(double thresh);
 
+    // This function defines how this layer can convert the output of a network
+    // into a label. Here, we just define a sample to be the same image if the
+    // distance is under a particular threhold.
     template <
         typename SUB_TYPE,
         typename label_iterator
         >
-    void to_label(
+    void to_label(                           // <interface>
         const dlib::tensor& input_tensor,
         const SUB_TYPE& sub,
         label_iterator iter
     ) const
     {
         const dlib::tensor& output_tensor = sub.get_output();
+
         DLIB_CASSERT(output_tensor.nr() == 1 && 
                      output_tensor.nc() == 1 ,"");
         DLIB_CASSERT(input_tensor.num_samples() == output_tensor.num_samples(),"");
 
+        // We iterate through each data pair and calculate distances; these are
+        // then compared with a label threshold.
         const float* out_data = output_tensor.host();
         long offset = output_tensor.k();
         for (long i = 0; i < output_tensor.num_samples()/2; i += 2) {
@@ -109,39 +163,55 @@ public:
         }
     }
 
+    // This function defines how this layer propagates the loss back to the rest
+    // of the network. Here, we will walk through how this is specifically done.
     template <
         typename const_label_iterator,
         typename SUBNET
         >
-    double compute_loss_value_and_gradient(
+    double compute_loss_value_and_gradient(  // <interface>
         const dlib::tensor& input_tensor,
         const_label_iterator truth,
         SUBNET& sub
     ) const
     {
+        // These calls get references to the output tensor of the network and
+        // the gradient to be passed back.
         const dlib::tensor& output_tensor = sub.get_output();
         dlib::tensor& grad = sub.get_gradient_input();
 
-        // Enforce contracts that define the loss layer interface. This is not
-        // necessary for generating this function, but it is good practice to
-        // check that contracts are obeyed by the code.
-        DLIB_CASSERT(input_tensor.num_samples() != 0,"");
-        DLIB_CASSERT(input_tensor.num_samples()%sample_expansion_factor == 0,"");
-        DLIB_CASSERT(input_tensor.num_samples() == grad.num_samples(),"");
-        DLIB_CASSERT(input_tensor.num_samples() == output_tensor.num_samples(),"");
+        // Enforce expectations of our data. For this application, we expect the
+        // data to be a vector across the channels. However, if the task was for
+        // semantic segmentation, the output could be an entire image instead.
         DLIB_CASSERT(output_tensor.nr() == 1 && 
                      output_tensor.nc() == 1,"");
         DLIB_CASSERT(grad.nr() == 1 && 
                      grad.nc() == 1,"");
 
-        const double scale = 2.0/output_tensor.num_samples();
+        // Enforce interface expectations.
+        DLIB_CASSERT(input_tensor.num_samples() != 0,"");
+        DLIB_CASSERT(input_tensor.num_samples()%sample_expansion_factor == 0,"");
+        DLIB_CASSERT(input_tensor.num_samples() == grad.num_samples(),"");
+        DLIB_CASSERT(input_tensor.num_samples() == output_tensor.num_samples(),"");
+
+        // Here, we implement the contrastive loss as described above. The
+        // things to note here are the pointers to the network data and it's
+        // gradient. The gradient is structured similar to other tensors in the
+        // library (i.e. columns, rows, channels, samples). Since, columns and
+        // rows are 1 for this application, jumping from sample to sample is
+        // simply the number of channels.
+        const double scale = 1.0/output_tensor.num_samples();
         double loss = 0;
         const float* out_data = output_tensor.host();
         float* g = grad.host();
         for (long i = 0; i < output_tensor.num_samples()/2; i += 2) {
             const float y = *truth++;
+
+            // Make sure the labels are 0 or 1
             DLIB_CASSERT(y == +1 || y == 0, "y: " << y);
 
+            // x1 and x2 are populated with the network outputs of the currently
+            // observed pair of samples.
             dlib::matrix<float,0,1> x1, x2;
             x1.set_size(output_tensor.k());
             x2.set_size(output_tensor.k());
@@ -150,19 +220,23 @@ public:
                 x2 = out_data[(i+1)*output_tensor.k()+k];
             }
 
-            float d = dlib::length(x1-x2);
+            float d = dlib::length(x1-x2); // euclidean distance of x1 and x2
             if (y) {
                 loss += d*d;
+                // NOTE: 2.0 comes from the derivative of squared(x1-x2)
+                float gscale = 2.0*scale;
                 for (long k = 0; k < output_tensor.k(); ++k) {
-                    g[i*output_tensor.k()+k] = scale*(x1(k)-x2(k));
-                    g[(i+1)*output_tensor.k()+k] = scale*(x2(k)-x1(k));
+                    g[i*output_tensor.k()+k] = gscale*(x1(k)-x2(k));
+                    g[(i+1)*output_tensor.k()+k] = gscale*(x2(k)-x1(k));
                 }
             }
             else {
                 float temp = margin-d;
+                // The following handles the max behavior
                 if (temp > 0) {
                     loss += temp*temp;
-                    float gscale = -scale*temp/(d+1e-4);
+                    // NOTE: 1e-4 prevents 0 division
+                    float gscale = -2.0*scale*temp/(d+1e-4);
                     for (long k = 0; k < output_tensor.k(); ++k) {
                         g[i*output_tensor.k()+k] = gscale*(x1(k)-x2(k));
                         g[(i+1)*output_tensor.k()+k] = gscale*(x2(k)-x1(k));
@@ -177,33 +251,20 @@ public:
             }
         }
 
-        return loss*scale;
+        return loss*scale; // return the average loss
     }
 
-    friend std::ostream& operator<<(std::ostream& out, const loss_contrastive_& item)
-    {
-        out << "loss_contrastive \t ("
-            << "margin=" << item.margin
-            << ", label_threshold=" << item.thresh
-            << ")";
-        return out;
-    }
-
-    friend void to_xml(const loss_contrastive_& item, std::ostream& out)
-    {
-        out << "<loss_contrastive"
-            << " margin='" << item.margin << "'"
-            << " label_threshold='" << item.thresh << "'/>";
-    }
-
-    friend void serialize(const loss_contrastive_& item, std::ostream& out)
+    // As with the input layer, these functions must be defined for our custom
+    // loss layer as well. Since we need to access internals of the layer, we
+    // define these functions as friend functions.
+    friend void serialize(const loss_contrastive_& item, std::ostream& out) // <interface>
     {
         dlib::serialize("loss_contrastive", out);
         dlib::serialize(item.margin, out);
         dlib::serialize(item.thresh, out);
     }
 
-    friend void deserialize(loss_contrastive_& item, std::istream& in)
+    friend void deserialize(loss_contrastive_& item, std::istream& in)      // <interface>
     {
         std::string version;
         dlib::deserialize(version, in);
@@ -212,13 +273,31 @@ public:
             dlib::deserialize(item.thresh, in);
         }
     }
+
+    friend std::ostream& operator<<(std::ostream& out, const loss_contrastive_& item) // <interface>
+    {
+        out << "loss_contrastive \t ("
+            << "margin=" << item.margin
+            << ", label_threshold=" << item.thresh
+            << ")";
+        return out;
+    }
+
+    friend void to_xml(const loss_contrastive_& item, std::ostream& out)    // <interface>
+    {
+        out << "<loss_contrastive"
+            << " margin='" << item.margin << "'"
+            << " label_threshold='" << item.thresh << "'/>";
+    }
 private:
     double margin;
     double thresh;
 };
 
+
+// The last implementation piece is to set an alias for our loss layer.
 template <typename SUBNET>
-using loss_contrastive = dlib::add_loss_layer<loss_contrastive_,SUBNET>;
+using loss_contrastive = dlib::add_loss_layer<loss_contrastive_,SUBNET>;    // <interface>
 
 // ---------------------------------------------------------------------------
 
@@ -230,9 +309,9 @@ void create_mnist_siamese_dataset(
     std::vector<unsigned char>& testing_labels
 )
 {
-    std::vector<dlib::matrix<unsigned char> > training_images_;
+    std::vector<dlib::matrix<unsigned char>> training_images_;
     std::vector<unsigned long> training_labels_;
-    std::vector<dlib::matrix<unsigned char> > testing_images_;
+    std::vector<dlib::matrix<unsigned char>> testing_images_;
     std::vector<unsigned long> testing_labels_;
     dlib::load_mnist_dataset(mnist_dir, training_images_, training_labels_, testing_images_,  testing_labels_);
 
@@ -303,8 +382,10 @@ int main(int argc, char* argv[]) try
                          dlib::relu<dlib::fc<500,
                          dlib::max_pool<2,2,2,2,dlib::relu<dlib::con<50,5,5,1,1,
                          dlib::max_pool<2,2,2,2,dlib::relu<dlib::con<20,5,5,1,1,
-                         input_image_pair> > > > > > > > > > >;
+                         input_image_pair>>>>>>>>>>>;
 
+    // Here, we set the parameters of the different layers. In order to have a
+    // similar network to 
     net_type net;
     dlib::layer<1>(net).layer_details().set_bias_learning_rate_multiplier(2);  // dlib::fc<2,...
     dlib::layer<2>(net).layer_details().set_bias_learning_rate_multiplier(2);  // dlib::fc<10,...
